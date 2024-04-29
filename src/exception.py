f6548a3cd330f85ee5e9e488373b8bb0185279cd
Nirmal-Yadagani <nirmalproffesionallife@gmail.com>
2024-04-29 15:57:15 +0530

    Update exception and logging scripts

--- src/exception.py
+++ src/exception.py
@@ -4,14 +4,21 @@ from logger import logging
 
 def error_message_detail(error, error_detail):
     _,_,exc_tb=error_detail.exc_info()
-    error_message="Error occured in python script [{0}] line number [{1}] error message [{2}]".format(exc_tb.tb_frame.f_code.co_filename, exc_tb.tb_lineno, str(error))
+    error_message=f"Error occured in python script [{exc_tb.tb_frame.f_code.co_filename}] line number [{exc_tb.tb_lineno}] error message [{str(error)}]"
     return error_message
 
 class CustomException(Exception):
     def __init__(self, error_message, error_detail):
-        super(CustomException, self).__init__(error_message)
+        super().__init__(error_message)
         self.error_message = error_message_detail(error_message,error_detail=error_detail)
 
 
     def __str__(self):
         return self.error_message
+    
+if __name__ == "__main__":
+    try:
+        a=1/0 
+    except Exception as e:
+        logging.info("Division by zero error")
+        raise CustomException(e, sys)
